4028ab7d366970cefa56006b49a9c7373b8ef2d1
Kevim <Kevim@KEVIM_NTBK>
2025-08-31 19:51:48 -0300

    atualização do app com inserção da searchbar dinamica, soma do valor dos produtos

--- AppMinhasCompras/Models/Produto.cs
+++ AppMinhasCompras/Models/Produto.cs
@@ -6,8 +6,9 @@ namespace AppMinhasCompras.Models
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public String Descricao { get; set; }
         public double Quantidade { get; set; }
         public double Preco { get; set; }
+        public double Total { get => Quantidade * Preco; }
     }
 }
